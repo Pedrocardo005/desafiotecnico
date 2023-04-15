6cf317053dab0e41c9c5a4b7fa87544f5de03908
Pedrocardo005 <pedrocardo007@hotmail.com>
2023-04-15 14:49:04 -0300

    Cria testes unitarios da api

--- desafioback/src/test/java/com/kounthub/desafioback/DesafiobackApplicationTests.java
+++ desafioback/src/test/java/com/kounthub/desafioback/DesafiobackApplicationTests.java
@@ -1,13 +1,41 @@
 package com.kounthub.desafioback;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kounthub.desafioback.models.Usuario;
+import com.kounthub.desafioback.service.UsuarioService;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DesafiobackApplicationTests {
+	
+	@Autowired
+	UsuarioService usuarioService;
 
 	@Test
-	void contextLoads() {
+	void verificaRetornoUsuarioLogin() {
+		Usuario usuario = usuarioService.getUsuario("Pedrocardo005");
+		assertNotNull(usuario);
+		assertNotNull(usuario.getRepositorios());
+	}
+	
+	@Test
+	void verificaRetornoUsuarioNaoExistente() {
+		Usuario usuario = null;
+		try {
+			usuario = usuarioService.getUsuario("@");
+		} catch (Exception e) {
+			
+		}
+
+		assertNull(usuario);
 	}
 
 }
