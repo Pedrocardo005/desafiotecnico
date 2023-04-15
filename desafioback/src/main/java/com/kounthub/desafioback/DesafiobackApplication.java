aaaa754d7c979fdf0bfd828024578a70e4886f26
Pedrocardo005 <pedrocardo007@hotmail.com>
2023-04-15 12:42:20 -0300

    First commit

---
+++ desafioback/src/main/java/com/kounthub/desafioback/DesafiobackApplication.java
@@ -0,0 +1,13 @@
+package com.kounthub.desafioback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobackApplication.class, args);
+	}
+
+}
---
+++ desafioback/src/test/java/com/kounthub/desafioback/DesafiobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kounthub.desafioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
